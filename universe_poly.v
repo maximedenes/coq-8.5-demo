d44bfbaf17b7e6a26856ee9f84356f88e51f932e
Matthieu Sozeau <mattam@mattam.org>
2014-01-08 12:58:46 +0100

    Add universe polymorphism demo file.

---
+++ universe_poly.v
@@ -0,0 +1,248 @@
+Require Import Utf8.
+
+Definition idmono {A : Type} (a : A) := a.
+
+Fail Definition selfapp : forall A, A -> A := 
+  idmono (@idmono).
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Polymorphic Definition id {A : Type} (a : A) := a.
+
+Definition selfapp : forall A, A -> A := id (@id).
+
+Set Printing All.
+Set Printing Universes.
+Print selfapp.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+(** Polymorphic inductives *)
+
+Polymorphic Inductive pprod (A B : Type) :=
+  | ppair (a : A) (b : B).
+
+Arguments ppair {A B} a b.
+
+Definition U2 := Type.
+Definition U1 := Type : U2.
+Definition U0 := Type : U1.
+
+Inspect 3.
+
+(* U2 -> Type 19 *)
+(* U1 -> Type 20 *)
+(* U0 -> Type 21 *)
+
+(** Polymorphic definitions *)
+
+Polymorphic Definition twice (u : Type) := 
+  pprod u u -> pprod u u.
+
+Let twiceU0 := twice U0.
+Let twiceU1 := twice U1.
+
+Print twiceU0.
+Print twiceU1.
+
+Check (twiceU1 : U2).
+Check (twiceU0 : U1).
+
+Fail Check (twiceU0 : U0).
+
+(* With cumulativity *)
+Check (twiceU0 : U2).
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+(** Types in records *)
+
+Module MonomorphicMagma.
+
+  Record magma := 
+    { magma_carrier :> Type;
+      magma_op : magma_carrier → magma_carrier → magma_carrier }.
+  
+  Definition magma_nat : magma := 
+    {| magma_carrier := nat;
+       magma_op x y := plus x y |}.
+  
+  Definition magma_product (x : magma) (y : magma) :=
+    {| magma_carrier := (x * y);
+       magma_op t u := 
+         let '(t1, t2) := t in
+         let '(u1, u2) := u in
+           (magma_op x t1 u1, magma_op y t2 u2)
+    |}.
+  
+  Fail Definition magma_magma : magma :=
+    {| magma_carrier := magma;
+       magma_op := magma_product |}.
+
+End MonomorphicMagma.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+(* With polymorphism *)
+
+Module PolymorphicMagma.
+  Set Universe Polymorphism.
+
+  Record magma := 
+    { magma_carrier :> Type;
+      magma_op : magma_carrier → magma_carrier → magma_carrier }.
+  
+  Definition magma_nat : magma := 
+    {| magma_carrier := nat;
+       magma_op x y := plus x y |}.
+  
+  Definition magma_product (x : magma) (y : magma) :=
+    {| magma_carrier := pprod x y;
+       magma_op t u := 
+         let 'ppair t1 t2 := t in
+         let 'ppair u1 u2 := u in
+           ppair (magma_op x t1 u1) (magma_op y t2 u2)
+    |}.
+  
+  Monomorphic Definition magma_magma : magma :=
+    {| magma_carrier := magma;
+       magma_op := magma_product |}.
+
+End PolymorphicMagma.
+
+(** Magma is instantiated at 2 incompatible levels in
+  [magma_magma] *)
+
+Print PolymorphicMagma.magma_magma.
+Test Universe Polymorphism.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+(** Internalized translations: *)
+
+Polymorphic Inductive sigma (A : Type) (P : A -> Type) :=
+  dpair : ∀ a : A, P a -> sigma A P.
+
+Notation " { x : A & P } " := (@sigma A (fun x : A => P)).
+
+
+Polymorphic Inductive paths (t := Type) 
+            (A : t) (a : A) : A -> t :=
+  idpath : paths A a a.
+
+Polymorphic Definition contr (A : Type) := 
+  forall x : A, { y : A | x = y }.
+
+Polymorphic Definition _Type := 
+  { A : Type & contr A }.
+
+Print _Type.
